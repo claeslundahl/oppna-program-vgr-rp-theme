92c851a792caea7e2e5af0ee0ae0855776b4b9bd
mrg3kko <mrg3kko@gmail.com>
2011-11-30 17:12:40 +0000

    Initial commit of new theme for regionportalen using maven with ref arch.

---
+++ core-bc/modules/theme/src/main/webapp/js/debug.js
@@ -0,0 +1,9 @@
+AUI().ready('aui-base', 'console', function(A) {
+	new A.Console({
+		//height: '250px',
+		newestOnTop: false,
+		//style: 'block',
+		visible: true//,
+		//width: '600px'
+	}).render();
+});
