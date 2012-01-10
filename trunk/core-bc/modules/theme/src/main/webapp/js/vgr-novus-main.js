e11a9c97dc8f24d4c146b94b4c57d6192405be31
mrg3kko@gmail.com <mrg3kko@gmail.com@33d570d6-0c6a-5a58-9177-be358fd4cc70>
2012-01-10 16:58:34 +0000

    Updates to news box carousel.

--- trunk/core-bc/modules/theme/src/main/webapp/js/vgr-novus-main.js
+++ trunk/core-bc/modules/theme/src/main/webapp/js/vgr-novus-main.js
@@ -88,6 +88,7 @@ AUI().add('vgr-novus-main',function(A) {
 					animSidebarPushX: 0,
 					computedHeaderHeight: '',
 					newsBoxCarousel: null,
+					newsBoxWrap: null,
 					
 					initializer: function(config) {
 						var instance = this;
@@ -168,11 +169,19 @@ AUI().add('vgr-novus-main',function(A) {
 						var instance = this;
 						
 						var newsBox = instance.get(NEWS_BOX);
+						var newsBoxWrap = newsBox.ancestor('.news-box-wrap');
+						
+						instance.newsBoxWrap = newsBoxWrap;
 						
 						if(isNull(newsBox)) {return;}
 						
 						var computedNewsBoxWidthStr = newsBox.getComputedStyle('width').replace('px', '');
 						var computedNewsBoxWidth = parseInt(computedNewsBoxWidthStr);
+
+						var height = 220;						
+						if(computedNewsBoxWidth < 540) {
+							height= 250;
+						}
 						
 						var newsBoxMenu = newsBox.ancestor().one('.news-box-menu');
 						
@@ -183,7 +192,7 @@ AUI().add('vgr-novus-main',function(A) {
 							contentBox: newsBox,
 							//activeIndex: 'rand',
 							activeIndex: 0,
-							height: 220,
+							height: height,
 							width: computedNewsBoxWidth,
 							nodeMenu: newsBoxMenu,
 							nodeMenuItemSelector: 'li'
@@ -191,6 +200,27 @@ AUI().add('vgr-novus-main',function(A) {
 
 						newsBox.all('a.news-box-link').removeClass('aui-helper-hidden');
 						newsBox.addClass('news-box-js');
+						
+						// Bind window size change event
+						A.on('window:resize', function(e) {
+							//var newsCarouselCB = instance.newsBoxCarousel.get('contentBox');
+							//var newsBoxWrap 
+							//console.log(newsCarouselCB);
+							//console.log('Window was resized.');
+							
+							var newsBoxWrap = instance.newsBoxWrap;
+							
+							if(isNull(newsBoxWrap)) {return;}
+							
+							var computedNewsBoxWrapWidthStr = newsBoxWrap.getComputedStyle('width').replace('px', '');
+							var computedNewsBoxWrapWidth = parseInt(computedNewsBoxWrapWidthStr);
+							
+							instance.newsBoxCarousel.set('width', computedNewsBoxWrapWidth);
+							
+							if(computedNewsBoxWrapWidth < 540) {
+								instance.newsBoxCarousel.set('height', 250);
+							}
+						});
 					},
 					
 					_initToggleHeader: function() {
@@ -537,6 +567,7 @@ AUI().add('vgr-novus-main',function(A) {
 			'aui-tooltip',
 			'anim',
 			'cookie',
+			'event-resize',
 			'substitute'
       ]
 	}
