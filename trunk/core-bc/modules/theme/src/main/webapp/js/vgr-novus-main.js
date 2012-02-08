3aeb65d433d208c6a8d6ed8527606b6dec5bc5ed
david.lgj@gmail.com <david.lgj@gmail.com@33d570d6-0c6a-5a58-9177-be358fd4cc70>
2012-02-08 07:54:07 +0000

    A better hack for instance id on kwowledgebase pages

--- trunk/core-bc/modules/theme/src/main/webapp/js/vgr-novus-main.js
+++ trunk/core-bc/modules/theme/src/main/webapp/js/vgr-novus-main.js
@@ -584,9 +584,8 @@ AUI().add('vgr-novus-main',function(A) {
 						//to the help page
 						var instance = this;
 						
-						e.stopImmediatePropagation();
 						e.halt();
-						
+															
 						var currentTarget = e.currentTarget;
 						var url = currentTarget.getAttribute('href');
 						
@@ -595,43 +594,52 @@ AUI().add('vgr-novus-main',function(A) {
 
 						if (portlet && portlet.hasClass('display_in_help')) {
 							//to display it in the help page we have to do a redirect,
-							//with a proper URL
+							//with a proper URL, the problem is that this URL needs a instance variable
 							//
 							//ie: from /knowledge_base_aggregator/tIa6/article/<id>/maximized
-							//to: /knowledge_base_display/YGi2/article/<id>
-							url = url.replace(/knowledge_base_aggregator\/.+\/article\/([0-9]+)\/?.*/,'knowledge_base_display/YGi2/article/$1');
-							window.location.href = url;
+							//to: /knowledge_base_display/<instance>/article/<id>
+							
+							var display = A.one('.knowledge-base-portlet-display');
+							if (display) {
+								var instance_id = display.attr('id').split('_INSTANCE_')[1];
+								if (instance_id) {
+									//remove trailing _ 
+									instance_id = instance_id.split('_')[0];
+									url = url.replace(/knowledge_base_aggregator\/.+\/article\/([0-9]+)\/?.*/,'knowledge_base_display/'+instance_id+'/article/$1');
+									window.location.href = url;
+									return false;
+								}
+							}
 									
-						} else {
+						} 
 						
-							var currentTitleNode = currentTarget.one('.taglib-text');
-							var currentTitle = currentTitleNode.html();
-						
-							url = url.replace('/group/', '/widget/group/');
-						
-							var dialogHeight = 500;
-							var dialogWidth = 700;
-						
-							var TPL_KB_IFRAME = '<div class="iframe-wrap"><iframe name="kbAggregatorDialog" id="kbAggregatorDialog" class="" title="" frameborder="0" src="{url}" width="{iframeWidth}" height="{iframeHeight}"></iframe></div>';
-						
-							var bodyContent = A.substitute(TPL_KB_IFRAME, {
-								iframeHeight: dialogHeight - 50,
-								iframeWidth: dialogWidth - 15,
-								url: url
-							});
-						
-							var dialog1 = new A.Dialog({
-								bodyContent: bodyContent,
-								centered: true,
-								constrain2view: true,
-								destroyOnClose: true,
-								height: dialogHeight,
-								modal: true,
-								width: dialogWidth,
-								title: currentTitle
-							}).render();
-						}
-						return false;
+						//open in modal window instead
+						var currentTitleNode = currentTarget.one('.taglib-text');
+						var currentTitle = currentTitleNode.html();
+					
+						url = url.replace('/group/', '/widget/group/');
+					
+						var dialogHeight = 500;
+						var dialogWidth = 700;
+					
+						var TPL_KB_IFRAME = '<div class="iframe-wrap"><iframe name="kbAggregatorDialog" id="kbAggregatorDialog" class="" title="" frameborder="0" src="{url}" width="{iframeWidth}" height="{iframeHeight}"></iframe></div>';
+					
+						var bodyContent = A.substitute(TPL_KB_IFRAME, {
+							iframeHeight: dialogHeight - 50,
+							iframeWidth: dialogWidth - 15,
+							url: url
+						});
+					
+						var dialog1 = new A.Dialog({
+							bodyContent: bodyContent,
+							centered: true,
+							constrain2view: true,
+							destroyOnClose: true,
+							height: dialogHeight,
+							modal: true,
+							width: dialogWidth,
+							title: currentTitle
+						}).render();
 					},
 
                     _onQuickNavigationClick: function(e) {
