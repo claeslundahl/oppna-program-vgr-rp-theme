db82b18fc93b1fdb1ba21069b9adc4489c4bd1e7
patrik.bergstrom@knowit.se <patrik.bergstrom@knowit.se@33d570d6-0c6a-5a58-9177-be358fd4cc70>
2012-08-21 08:58:19 +0000

    Added rp-patient-context.js which is used by several portlets. Modified calendar style in custom.css.

---
+++ trunk/core-bc/modules/theme/src/main/webapp/js/rp-patient-context.js
@@ -0,0 +1,45 @@
+function pollForNewPatient(url) {
+    const A = AUI();
+    A.io.request(url, {
+        cache:false,
+        sync:false,
+        timeout:60 * 1000,
+        method:'post',
+        on:{
+            success:function () {
+                var response = this.get('responseData');
+                if (response == 'update=true') {
+                    var dialog = new A.Dialog({
+                        bodyContent:'Aktuell patient har uppdaterats. Vill du ladda om sidan och visa den nya patienten? Observera att nästa gång du laddar om sidan kommer den nya patienten visas.',
+                        buttons:[
+                            {
+                                text: 'Ja, ladda om sidan',
+                                handler:function () {
+                                    window.location.reload();
+                                }
+                            },
+                            {
+                                text: 'Nej, jag vill behålla den patient som syns på skärmen',
+                                handler: function() {
+                                    pollForNewPatient(url);
+                                    this.close();
+                                }
+                            }
+                        ],
+                        centered:true,
+                        draggable:true,
+                        modal:true,
+                        title:'Patientkontext uppdaterad'
+                    }).render();
+                } else if (response == 'update=false') {
+                    pollForNewPatient(url);
+                } else {
+                    // Something probably went wrong, e.g. session was invalidated so the client is not logged in. Quit sending requests.
+                }
+            },
+            failure:function () {
+                pollForNewPatient(url); // On timeout e.g.
+            }
+        }
+    });
+}
